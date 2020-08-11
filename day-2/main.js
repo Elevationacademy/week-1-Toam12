bc4ed30b6e4b10e8d1263ed1080eaa40e54609d5
toam <toamcohen.transformation@gmail.com>
2020-08-11 15:53:16 +0300

    first

---
+++ day-2/main.js
@@ -0,0 +1,58 @@
+// var = value "old value";
+// let = value ;
+// const = value "constent value";
+
+// console=Object
+// log=method;
+// console.log("hello world");
+
+
+// const green = ()=>{
+// 	console.log(`hello, i d'ont panic`)
+// }
+let someName = `string`;
+console.log(someName);
+
+let number = 15
+number = 15 
+const undifinde = `cant change that content inside const im this situation and i cant also change the varbel is self undifine cannot change`;
+
+
+let boolean = true;
+boolean = false;
+
+
+const first = "string "
+const second = "NaN"
+console.log(first + second)
+first +="yes"
+console.log(first)
+
+`string plus Number its string`
+
+console.log (1===10)
+console.log (1==10)
+console.log (1!==10)
+console.log (1!=10)
+console.log (1>10)
+console.log (1<10)
+console.log (1<=10)
+console.log (1>=10)
+
+
+1=="1" true;
+1==="1" false;
+
+
+	console.log(10+2)
+	console.log(10/2)
+ 	console.log(10-2)
+	console.log(10*2)
+
+
+
+	console.log(20%3) //give us 2
+
+
+	const x = 41223
+console.log(x % 2 == 0)
---
+++ day-3/mainAO.js
@@ -0,0 +1,292 @@
+// const people = ["jena", "dor", "albert", "ronit",];
+
+// for(let peopleLoop in people){
+// 	console.log (people[peopleLoop])
+// }
+
+// for(let peopleLoop in people){
+	// 	console.log (peopleLoop)
+	// }
+	
+	
+const people = ["jena", "dor", "albert", "ronit",];
+for(let x of people){
+	console.log(`One day I will work at ${x}` ) 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const genes = ["ACADM", "GLMN", "RLF", "AZIN2", "CRYZ"]
+// const genes1 = genes.concat()
+// console.log(genes1)
+// genes1.splice(1 -1, 0, 'FXT')
+// console.log(genes1)
+// genes1.splice(4 -1, 0, 'CRYZ')
+// console.log(genes1)
+// let lastIndex2 = genes1.length -1;
+// genes1[lastIndex2] = "AZIN2"
+// console.log(genes1)
+// 	let obj ={
+// 		color:"string",
+// 		numWheels:10,
+// 		isFancy:true,
+// 	};
+		
+// 	let obj1 = {
+// 		color: "tototo",
+// 		boolean: true,
+// 		items:["stam", "ani", "cotev",]
+// 	};
+// 	console.log(obj1["color"])
+
+
+// 	let objects = [obj , obj1];
+// 	console.log(objects[1].color)
+
+// 	obj.color = "not astring"
+// 	console.log(obj)
+
+
+// 	let a = obj
+
+
+
+
+
+
+
+// let p1 = {
+// 	name: "robert",
+// 	age: 20,
+// 	city: "holon"
+	
+// };
+
+// let p2 = {
+// 	name: "jill",
+// 	age: 2,
+// 	city: "hoon"
+	
+// };
+
+// if (p1.age===p2.age || p1.city===p2.city){
+// 	console.log("Jill wanted to date Robert")
+// }
+// else{
+// 	console.log("Jill wanted to date Robert, but couldn't")
+// }
+
+
+
+
+
+
+// 	let myList = [
+// 		{
+// 			username: "toam",
+// 			password: "1234",
+// 			spells:["Shazam", "Abrakdbra", "Hokospokos", "Boo",]
+// 		},
+
+// 		{
+// 			username: "toam",
+// 			password: "1234",
+// 			spells: ["ditm", "ican", "iknow",]
+// 		},
+
+// 	];
+
+
+
+// 		myList[0].spells[3] = "Shlagazem";
+// 		console.log (myList)
+// 		myList[0].spells.splice(1,1)
+// 		console.log(myList)
+
+
+
+	
+
+
+// let myList2 = [
+// 	{
+// 		user:"toam",
+// 		player:"fighter",
+// 	},
+
+
+// 	{
+// 		user:"toam",
+// 		player:"assasin",
+// 	}
+// ];
+
+// myList.push(...myList2);
+
+// console.log(myList);
+
+
+
+
+
+// let library = {
+// 	books:[
+// 		{
+// 			title:"spidermen",
+// 			author:"i dont know :)"
+// 		},
+
+// 		{
+// 			title:"green-arrow",
+// 			author:"i dont know :)"
+// 		},
+
+// 		{
+// 			title:"cooke-arrow",
+// 			author:"i dont know :)"
+// 		},
+// 	],
+
+// }
+
+// myList.push(library.books[0])
+// console.log(myList)
+
+
+// const myStuff =[
+// 	"keys",
+// 	"wood",
+// 	"home",
+// 	"pens",
+
+// ];
+
+
+// const notMyStuff =[
+// 	"keys",
+// 	"wood",
+// 	"home",
+// 	"pens",
+
+// ];
+
+
+// console.log(myStuff[1])//Give me the "wood";
+// console.log(myStuff.length)//Give me 4
+// //index = [0, 1, 2, 3,]
+// const lastIndex = (myStuff.length-1);//Give me the lest index
+// // console.log(myStuff[lastIndex]); //Give me the last item
+// console.log(myStuff[lastIndex]+ myStuff[0])
+// myStuff[1] = "world";
+// myStuff.push("helloooooo");
+// console.log(myStuff)
+// myStuff.unshift("helloooooo2");
+// console.log(myStuff)
+// myStuff.push("add1", "add2", "add3",)
+// console.log(myStuff)
+
+// // myStuff.push(notMyStuff)
+// // console.log(myStuff)
+
+// myStuff.push(...notMyStuff)
+// console.log(myStuff)
+// console.log(notMyStuff)
+// myStuff.splice(1,5)//should delete from point 1 5 items
+// console.log(myStuff)
+// // splice(1=index , 5=how much do you want to delete)
+// myStuff.splice(1 -1 ,0, 'hey1')
+// console.log(myStuff)
+// let removeItems = myStuff.splice(3,1)
+// console.log(removeItems);
+// console.log(myStuff)
+// removeItems = myStuff.splice(1)
+// console.log(removeItems)//Remove 1 from the index 1 
+// console.log(myStuff)//make the array with the only index 1 
+
+
+
+// const lastIndex1 = myStuff.length-1;
+// console.log(myStuff[lastIndex1])
+// let removeBe = myStuff.splice(lastIndex1)
+// console.log(myStuff)
+// console.log(removeBe)
